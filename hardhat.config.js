fcb8f2336cc5ee5098bb41dde2ad8f7e34f0181b
Satyadeepsb <satyadeepsb@gmail.com>
2022-11-09 13:05:41 +0530

    deployed contract hardhat with goeril network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+
+const GOERIL_RPC_URL = process.env.GOERIL_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERIL_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
   solidity: "0.8.17",
 }
